c31744ef02a13acedd4c36af94c2e3e2c6d22903
Brendan838 <brendan@Brendans-iMac.local>
2021-07-26 21:30:03 -0500

    added Giphy API

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Declarations of all buttons
+
+
+
 let showScreen = function (screen) {
   //prevent page from reloading on form submission
   window.event.preventDefault();
@@ -16,3 +20,39 @@ let showScreen = function (screen) {
   //display menu, trivia, High Scores sections when click
   document.getElementById(screen).style.display = "block";
 };
+
+//All functions on page load
+ 
+
+
+
+
+//Event Functionality of all Buttons
+
+
+
+
+//API for getting a random GIF from a max list of 25 based on the search criteria entered. Will attach source to any element entered
+
+function getGIF(searchItem, gifEl) {
+
+  var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=0pXpAzRY9RmZGboXjvmC9uwTPKv6JApT&q=" + searchItem + "&limit=25&offset=0&rating=pg-13&lang=en"
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    console.log(data)
+    var i = Math.floor(Math.random() * data.data.length)
+    console.log(i)
+    var randomGif = data.data[i].images.downsized.url
+    gifEl.setAttribute("src", randomGif)
+    })
+} 
+
+
+
+//other functions
+
+
